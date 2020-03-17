f52eb6292bb3912221745f16f26c67c4ac0e3797
Satyam Sangal <satyamsangal95@gmail.com>
2020-03-17 18:36:05 +0530

    First Commit

---
+++ lib/choose_screen.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:loginui/login_page.dart';
+class ChooseScreen extends StatefulWidget {
+  @override
+  _ChooseScreenState createState() => _ChooseScreenState();
+}
+
+class _ChooseScreenState extends State<ChooseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage("https://ssl.c.photoshelter.com/img-get/I0000J3iCILMoNNI/s/880/704/White-Mountain-Portrait-1.jpg",),
+                    fit: BoxFit.fill
+                ),
+              ),
+            ),
+
+            Container(
+              color: Colors.red.withOpacity(0.85),
+            ),
+
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  // crossAxisAlignment: CrossAxisAlignment.end,
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.favorite,size: 60,color: Colors.white,),
+                        RichText(
+                          text: TextSpan(
+                              text: '',
+                              children: <TextSpan>[
+                                TextSpan(text: 'heart',style: TextStyle(fontSize: 40)),
+                                TextSpan(text: 'link',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40)),
+                              ]
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Column(
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: (){},
+                          child: Container(
+                            height: 40,
+                            width: 270,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.white,
+                                  style: BorderStyle.solid,
+                                  width: 1.5
+                              ),
+                              //color: Colors.red,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text('SIGN UP',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 20,),
+
+
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+
+                      },
+                      child: Container(
+                        height: 40,
+                        width: 270,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                              color: Colors.white,
+                              style: BorderStyle.solid,
+                              width: 1.5
+                          ),
+                          //color: Colors.red,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Center(
+                          child: Text('LOGIN',style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),),
+                        ),
+                      ),
+                    ),
+                      ],
+                    ),
+
+
+//                  RaisedButton(
+//                  shape: RoundedRectangleBorder(
+//                    borderRadius: BorderRadius.circular(18),
+//                    side: BorderSide(color: Colors.white),
+//                  ),
+//                    child: Text('SIGN UP',style: TextStyle(color: Colors.white),),
+//                  ),
+//
+//                  RaisedButton(
+//                    shape: RoundedRectangleBorder(
+//                      borderRadius: BorderRadius.circular(18),
+//                      side: BorderSide(color: Colors.white),
+//                    ),
+//                    child: Text('LOGIN',style: TextStyle(color: Colors.white),),
+//                  )
+
+                  ],
+                ),
+              ),
+            ),
+
+
+
+
+
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login_page.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:loginui/fb_login.dart';
+import 'package:loginui/google_login.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                      "https://ssl.c.photoshelter.com/img-get/I0000J3iCILMoNNI/s/880/704/White-Mountain-Portrait-1.jpg",
+                    ),
+                    fit: BoxFit.fill),
+              ),
+            ),
+            Container(
+              color: Colors.white.withOpacity(0.9),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(50),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(
+                      Icons.favorite,
+                      size: 70,
+                      color: Colors.red,
+                    ),
+
+                    SizedBox(height: 5,),
+
+                    Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'USERNAME',
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red)),
+                                labelStyle: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.bold),
+                            hintText: 'USERNAME',
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color : Colors.red)
+                            )
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'PASSWORD',
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red)),
+                            labelStyle: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.bold),
+                            hintText: 'PASSWORD',
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color : Colors.red),
+                              )
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              'Forget Password?',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            height: 40,
+                            width: 270,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                  color: Colors.white,
+                                  style: BorderStyle.solid,
+                                  width: 1.5),
+                              //color: Colors.red,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'LOGIN',
+                                style: TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(children: <Widget>[
+                          Expanded(
+                            child: new Container(
+                                margin: const EdgeInsets.only(
+                                    left: 10.0, right: 15.0),
+                                child: Divider(
+                                  color: Colors.black,
+                                  height: 50,
+                                )),
+                          ),
+                          Text("OR CONNECT WITH"),
+                          Expanded(
+                            child: new Container(
+                                margin: const EdgeInsets.only(
+                                    left: 15.0, right: 10.0),
+                                child: Divider(
+                                  color: Colors.black,
+                                  height: 50,
+                                )),
+                          ),
+                        ]),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+
+                            GestureDetector(
+                              onTap: () {
+
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => FbLogin()));
+
+                              },
+                              child: Container(
+                                height: 40,
+                                width: 130,
+                                decoration: BoxDecoration(
+                                  color: Colors.blue[900],
+                                  border: Border.all(
+                                      color: Colors.blue[900],
+                                      style: BorderStyle.solid,
+                                      width: 1.5),
+                                  //color: Colors.red,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Image.network("https://i.ya-webdesign.com/images/white-page-dividers-png-4.png"),
+
+                                    Text(
+                                      'FACEBOOK',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+
+                            GestureDetector(
+                              onTap: () {
+
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => GoogleLogin()));
+
+                              },
+                              child: Container(
+                                height: 40,
+                                width: 130,
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  border: Border.all(
+                                      color: Colors.red,
+                                      style: BorderStyle.solid,
+                                      width: 1.5),
+                                  //color: Colors.red,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    SizedBox(width: 10,),
+                                    Image.network("https://www.diag.ai/img/gplus.png"),
+
+                                    Text(
+                                      'GOOGLE',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+             ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
